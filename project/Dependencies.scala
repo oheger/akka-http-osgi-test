99fc890976ec96c89e07e3b2f9049b587b539daf
oheger <oliver.heger@oliver-heger.de>
2018-05-19 21:58:00 +0200

    Added dependencies for PaxExam.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,25 @@
 import sbt._
 
 object Dependencies {
+  lazy val VersionPaxExam = "4.11.0"
+  lazy val VersionPaxUrl = "2.5.4"
+  lazy val VersionLogBack = "1.2.3"
+  lazy val VersionFelix = "5.6.10"
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
 
   lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.0.10"
+
+  lazy val paxExam = Seq(
+    "org.ops4j.pax.exam" % "pax-exam-container-native" % VersionPaxExam % Test,
+    "org.ops4j.pax.exam" % "pax-exam-junit4" % VersionPaxExam % Test,
+    "org.ops4j.pax.exam" % "pax-exam-link-mvn" % VersionPaxExam % Test,
+    "org.ops4j.pax.url" % "pax-url-aether" % VersionPaxUrl % Test,
+    "org.ops4j.pax.url" % "pax-url-classpath" % VersionPaxUrl % Test,
+    "org.ops4j.pax.url" % "pax-url-link" % VersionPaxUrl % Test,
+    "ch.qos.logback" % "logback-core" % VersionLogBack % Test,
+    "ch.qos.logback" % "logback-classic" % VersionLogBack % Test
+  )
+
+  lazy val felix = "org.apache.felix" % "org.apache.felix.framework" % VersionFelix % Test
 }
